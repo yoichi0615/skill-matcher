77225f384fa3326f8bb58ef654bc1efdcfc20261
yo0615 <aaa11655pt@gmail.com>
2022-04-24 19:00:10 +0900

    post_reviewsテーブル追加

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -12,4 +12,8 @@ class Post extends Model
     {
       return $this->belongsTo(User::class);
     }
+
+    public function post_reviews() {
+        return $this->hasMany(PostReview::class);
+    }
 }
---
+++ database/migrations/2022_04_24_183111_create_post_reviews_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostReviewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post_reviews', function (Blueprint $table) {
+            $table->id();
+            $table->integer('post_id')->default(0);
+            $table->unsignedBigInteger('user_id')->default(0);
+            $table->integer('stars')->default(0)->comment('星');
+            $table->text('comment')->comment('コメント');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('post_reviews');
+    }
+}

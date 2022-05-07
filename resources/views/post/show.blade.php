3022a55e9277c4c41036be7f1d08780aed88d5df
yoichi0615 <aaa11655pt@gmail.com>
2022-05-07 18:03:40 +0900

    Carbonインストール

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -3,16 +3,25 @@
 <div>
   これはユーザー（投稿の詳細ページです。）
 </div>
-@if (\Auth::user()->id === $post->user_id)
-    <a href="{{ route('post.edit' )}}">投稿を編集</a>
-@endif
+@auth
+  @if (\Auth::user()->id === $post->user_id)
+      <a href="{{ route('post.edit' )}}">投稿を編集</a>
+  @endif
+@endauth
+@php
+    $now = new \Carbon\Carbon();
+@endphp
 
 <section>
   <div class="container">
     <div class="col-md-8 item-frame">
       <div class="item-heder">
-        <span>これが所属する分類（お金、英語、プログラミング、etc...）（仮置き）</span>
-        <h1 class="item-header-title">募集タイトル</h1>
+        <span>
+          @foreach ($post->tags as $tag)
+            {{$tag->hash_tag}}
+          @endforeach
+        </span>
+        <h1 class="item-header-title">{{ $post->summary }}</h1>
         <h3 class="item-header-want">求めるスキル</h3>
         <span>所在地（必要であれば）</span>
       </div>
@@ -25,10 +34,10 @@
         <figure>
           <ul>
             <li>
-              <span style="font-size:12px;">発行日（仮置き）</span>
-              <span style="font-size:15px;">4ヶ月前（仮置き）</span>
-              <span style="font-size:12px;">最終更新（仮置き）</span>
-              <span style="font-size:15px;">2ヶ月前（仮置き）</span>
+              <span style="font-size:12px;">発行日</span>
+              <span style="font-size:15px;">{{ \Carbon\Carbon::parse($post->created_at)->format('Y-m-d') }} ({{$now->diffInDays($post->created_at)}}日前)</span>
+              <span style="font-size:12px;">最終更新</span>
+              <span style="font-size:15px;">{{ \Carbon\Carbon::parse($post->updated_at)->format('Y-m-d') }} ({{$now->diffInDays($post->created_at)}}日前)</span>
             </li>
             <li>
               <div>タグを挿入（仮置き）</div>
